9d0cc3bbe485b175922c1949dd156790a03b30e3
Chen Ben Ami <chen24201@gmail.com>
2020-10-04 02:31:30 +0300

    Initial commit

---
+++ src/test/java/com/example/kafkaMessages/KafkaMessagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaMessages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMessagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
